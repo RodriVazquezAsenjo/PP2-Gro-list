a9ff522eb63f3b69ec9ade2de30238dc2ef25810
Rodrigo Vazquez <r.vazquezasenjo@gmail.com>
2024-08-08 18:17:31 +0000

    pantry items start adding when the pantry form is submitted - styling still needed

--- script.js
+++ script.js
@@ -11,17 +11,6 @@ function openNav() {
     
 }
 
-function openWindow() {
-    let ingredientForm = document.getElementById('add-ingredient-form');
-    if (ingredientForm.classList.contains('visible')) {
-        ingredientForm.classList.remove('visible');
-        ingredientForm.style.opacity = '0';
-    } else {
-        ingredientForm.classList.add('visible');
-        ingredientForm.style.opacity = '1';
-    }
-}
-
 function closeWindow() {
     let ingredientForm = document.getElementById('add-ingredient-form');
     if (ingredientForm.classList.contains('visible')) {
@@ -32,3 +21,29 @@ function closeWindow() {
         ingredientForm.style.opacity = '1';
     }
 }
+
+function addIngredients(event){
+    event.preventDefault();
+
+    let ingredientDetails = [];
+    let ingredient = document.getElementById('add-ingredient-text').value;
+    let quantity = document.getElementById('add-ingredient-quantity').value;
+    
+    ingredientDetails.push(ingredient);
+    ingredientDetails.push(quantity);
+    
+    for (let i=0; i<ingredientDetails.length; i++) {
+        let pantryItem = document.createElement('div');
+        pantryItem.className = 'pantry-item';
+        pantryItem.innerHTML =`
+        <div class="pantry-text">
+            <span class="counter">${ingredientDetails[i+1]}</span>
+            <h3 class="pantry-title">${ingredientDetails[0]}</h3>
+        </div>`
+        ;
+        document.getElementById('pantry-list').appendChild(pantryItem);
+    }    
+    console.log(ingredientDetails);
+}
+document.getElementById('button-add').addEventListener('submit', addIngredients);
+addIngredients();
